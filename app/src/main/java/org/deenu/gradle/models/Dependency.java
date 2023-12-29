6afe60de5dd96fdcdd4a6eab73708599fc60f8e4
Deenu143 <deenbandhunetam488@gmail.com>
2023-12-29 16:25:14 +0000

    format codes

--- app/src/main/java/org/deenu/gradle/models/Dependency.java
+++ app/src/main/java/org/deenu/gradle/models/Dependency.java
@@ -1,64 +1,65 @@
 package org.deenu.gradle.models;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Dependency {
 
-	private String configuration;
-	private String group;
-	private String name;
-	private String version;
+  private String configuration;
+  private String group;
+  private String name;
+  private String version;
 
-	public Dependency(String group, String name, String version) {
-		this.group = group;
-		this.name = name;
-		this.version = version;
-	}
+  public Dependency(String group, String name, String version) {
+    this.group = group;
+    this.name = name;
+    this.version = version;
+  }
 
-	public Dependency() {
-	}
+  public Dependency() {}
 
-	public static Dependency fromString(String dependency) {
-		String[] values = dependency.split(":");
-		return new Dependency(values[0], values[1], values[2]);
-	}
+  public static Dependency fromString(String dependency) {
+    String[] values = dependency.split(":");
+    return new Dependency(values[0], values[1], values[2]);
+  }
 
-	public void setConfiguration(String configuration) {
-		this.configuration = configuration;
-	}
+  public void setConfiguration(String configuration) {
+    this.configuration = configuration;
+  }
 
-	public String getConfiguration() {
-		return ((configuration != null) ? configuration : "configuration");
-	}
+  public String getConfiguration() {
+    return ((configuration != null) ? configuration : "configuration");
+  }
 
-	public void setGroup(String group) {
-		this.group = group;
-	}
+  public void setGroup(String group) {
+    this.group = group;
+  }
 
-	public String getGroup() {
-		return group;
-	}
+  public String getGroup() {
+    return group;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setVersion(String version) {
-		this.version = version;
-	}
+  public void setVersion(String version) {
+    this.version = version;
+  }
 
-	public String getVersion() {
-		return version;
-	}
+  public String getVersion() {
+    return version;
+  }
 
-	@Override
-	public String toString() {
-		return ((configuration != null) ? configuration : "configuration") + ":" + group + ":" + name + ":" + version;
-	}
-
+  @Override
+  public String toString() {
+    return ((configuration != null) ? configuration : "configuration")
+        + ":"
+        + group
+        + ":"
+        + name
+        + ":"
+        + version;
+  }
 }
--- app/src/main/java/org/deenu/gradle/parser/Main.java
+++ app/src/main/java/org/deenu/gradle/parser/Main.java
@@ -1,57 +1,60 @@
 package org.deenu.gradle.parser;
 
 import java.io.File;
-import org.deenu.gradle.models.Dependency;
 import org.deenu.gradle.parser.exception.ParserFailedException;
 import org.deenu.gradle.script.GradleScript;
 
 public class Main {
 
-	public static void main(String[] args) {
-		try {
+  public static void main(String[] args) {
+    try {
 
-			File buildGradle = new File("/storage/emulated/0/test/build.gradle");
-			GradleScript gradleScript = new GradleScript(buildGradle);
-	
-			System.out.println();
-			System.out.println("GradleFile: " + gradleScript.getGradleFile());
-			System.out.println();
-			System.out.println("GradleFileName: " + gradleScript.getGradleFileName());
-			System.out.println();
-			System.out.println("IsGradleBuildFile: " + gradleScript.isGradleBuildFile());
-			System.out.println();
-			System.out.println("IsGradleSettingsFile: " + gradleScript.isGradleSettingsFile());
-			System.out.println();
-			System.out.println("Plugins: " + gradleScript.getPlugins());
-			System.out.println();
-			System.out.println("Repositories: " + gradleScript.getRepositories());
-			System.out.println();
-			System.out.println("BuildScriptRepositories: " + gradleScript.getBuildScriptRepositories());
-			System.out.println();
-			System.out.println("AllProjectsRepositories: " + gradleScript.getAllProjectsRepositories());
-			System.out.println();
-			System.out.println(
-					"AllProjectsRepositoriesFlatDirDirs: " + gradleScript.getAllProjectsRepositoriesFlatDirDirs());
-			System.out.println();
-			System.out.println("PluginManagementRepositories: " + gradleScript.getPluginManagementRepositories());
-			System.out.println();
-			System.out.println("DependencyResolutionManagementRepositories: "
-					+ gradleScript.getDependencyResolutionManagementRepositories());
-			System.out.println();
-			System.out.println("DependencyResolutionManagementRepositoriesFlatDirDirs: "
-					+ gradleScript.getDependencyResolutionManagementRepositoriesFlatDirDirs());
-			System.out.println();
-			System.out.println("rootProjectName: " + gradleScript.getRootProjectName());
-			System.out.println();
-			System.out.println("Include: " + gradleScript.getIncludes());
-			System.out.println();
-			System.out.println("Dependencies: " + gradleScript.getDependencies());
-			System.out.println();
-			System.out.println("BuildScriptDependencies: " + gradleScript.getBuildScriptDependencies());
-			System.out.println();
-			
-		} catch (Exception e) {
-			System.out.println(new ParserFailedException(e.getMessage()));
-		}
-	}
+      File buildGradle = new File("/storage/emulated/0/test/build.gradle");
+      GradleScript gradleScript = new GradleScript(buildGradle);
+
+      System.out.println();
+      System.out.println("GradleFile: " + gradleScript.getGradleFile());
+      System.out.println();
+      System.out.println("GradleFileName: " + gradleScript.getGradleFileName());
+      System.out.println();
+      System.out.println("IsGradleBuildFile: " + gradleScript.isGradleBuildFile());
+      System.out.println();
+      System.out.println("IsGradleSettingsFile: " + gradleScript.isGradleSettingsFile());
+      System.out.println();
+      System.out.println("Plugins: " + gradleScript.getPlugins());
+      System.out.println();
+      System.out.println("Repositories: " + gradleScript.getRepositories());
+      System.out.println();
+      System.out.println("BuildScriptRepositories: " + gradleScript.getBuildScriptRepositories());
+      System.out.println();
+      System.out.println("AllProjectsRepositories: " + gradleScript.getAllProjectsRepositories());
+      System.out.println();
+      System.out.println(
+          "AllProjectsRepositoriesFlatDirDirs: "
+              + gradleScript.getAllProjectsRepositoriesFlatDirDirs());
+      System.out.println();
+      System.out.println(
+          "PluginManagementRepositories: " + gradleScript.getPluginManagementRepositories());
+      System.out.println();
+      System.out.println(
+          "DependencyResolutionManagementRepositories: "
+              + gradleScript.getDependencyResolutionManagementRepositories());
+      System.out.println();
+      System.out.println(
+          "DependencyResolutionManagementRepositoriesFlatDirDirs: "
+              + gradleScript.getDependencyResolutionManagementRepositoriesFlatDirDirs());
+      System.out.println();
+      System.out.println("rootProjectName: " + gradleScript.getRootProjectName());
+      System.out.println();
+      System.out.println("Include: " + gradleScript.getIncludes());
+      System.out.println();
+      System.out.println("Dependencies: " + gradleScript.getDependencies());
+      System.out.println();
+      System.out.println("BuildScriptDependencies: " + gradleScript.getBuildScriptDependencies());
+      System.out.println();
+
+    } catch (Exception e) {
+      System.out.println(new ParserFailedException(e.getMessage()));
+    }
+  }
 }
